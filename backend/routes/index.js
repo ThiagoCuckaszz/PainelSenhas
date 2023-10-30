392a476bd483a08a8c1cbe689d04fc0e4b90c7df
ThiagoCuckaszz <pessoalcuckasz@gmail.com>
2023-10-30 09:36:58 -0300

    Não zera fichas

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,79 +4,98 @@ const ThermalPrinter = require("node-thermal-printer").printer;
 const PrinterTypes = require("node-thermal-printer").types;
 const sqlite3 = require('sqlite3').verbose();
 
-const dataHoraAtual = new Date();
-const dataHoraFormatada = dataHoraAtual.toLocaleString();
-
-let numeroFicha = 0;
-
 const db = new sqlite3.Database('fichas.db');
-
 db.serialize(function () {
-  db.run('CREATE TABLE IF NOT EXISTS fichas (id INTEGER PRIMARY KEY AUTOINCREMENT, tipo TEXT, numero INT)');
+  db.run('CREATE TABLE IF NOT EXISTS fichas (id INTEGER PRIMARY KEY AUTOINCREMENT, tipo TEXT, numero INT, hora DATETIME)');
 });
 
-function gerarFicha(tipoFicha, res) {
-  db.get('SELECT MAX(numero) AS max_numero FROM fichas WHERE tipo = ?', tipoFicha, function (err, row) {
-    if (err) {
-      res.status(500).send('Erro ao consultar a próxima ficha: ' + err.message);
-      return;
-    }
+// Utilize um objeto para rastrear o número de ficha por tipo
+let numeroFichas = {
+  Consulta: 0,
+  Preventivo: 0,
+  "Exames Laboratoriais": 0,
+  "Exames Não Laboratoriais": 0,
+};
 
-    if (row && row.max_numero !== null) {
-      numeroFicha = row.max_numero + 1;
-    } else {
-      numeroFicha = 1;
-    }
+let dataUltimaFicha = new Date().toLocaleDateString();
 
-    const ficha = `${tipoFicha.substr(0, 1)}${numeroFicha}`;
-
-    // Configura a impressora
-    let printer = new ThermalPrinter({
-      type: PrinterTypes.EPSON,
-      interface: 'tcp://192.168.10.31'
-    });
-
-    // Imprime a ficha atual
-    printer.alignCenter();
-    printer.setTextSize(1, 1);
-    printer.println("Fichas");
-    printer.newLine();
-    printer.println('-------------------');
-    printer.println(tipoFicha);
-    printer.underlineThick(false);
-    printer.newLine();
-    printer.println(`${ficha}`);
-    printer.newLine();
-    printer.setTextSize(0, 0);
-    printer.println(dataHoraFormatada);
-    printer.cut();
-
-    try {
-      printer.execute();
-      console.log("Impressão concluída!");
-      res.send(`Impressão concluída para a ficha de ${tipoFicha} ${ficha}`);
-      db.run('INSERT INTO fichas (tipo, numero) VALUES (?, ?)', tipoFicha, numeroFicha);
-    } catch (error) {
-      console.error("Falha na impressão:", error);
-      res.status(500).send("Falha na impressão: " + error.message);
+// Função para obter a última ficha impressa do banco de dados
+function obterUltimaFichaImprimida() {
+  db.all('SELECT tipo, MAX(numero) as lastFicha FROM fichas GROUP BY tipo', function (err, rows) {
+    if (!err && rows) {
+      rows.forEach((row) => {
+        if (row.tipo && row.lastFicha) {
+          numeroFichas[row.tipo] = row.lastFicha;
+        }
+      });
     }
   });
 }
 
+// Chame a função para obter a última ficha impressa ao inicializar o aplicativo
+obterUltimaFichaImprimida();
+
+function gerarFicha(tipoFicha, res) {
+  const dataAtual = new Date().toLocaleDateString();
+
+  if (dataAtual !== dataUltimaFicha) {
+    // A data mudou desde a última ficha
+    dataUltimaFicha = dataAtual; // Atualize a data da última ficha
+    obterUltimaFichaImprimida(); // Recarregue o número de fichas a partir do banco de dados
+  } else {
+    // Ainda é o mesmo dia, apenas incremente o número da ficha
+    numeroFichas[tipoFicha]++;
+  }
+
+  const ficha = `${numeroFichas[tipoFicha].toString().padStart(3, '0')}`;
+
+  // Configura a impressora
+  let printer = new ThermalPrinter({
+    type: PrinterTypes.EPSON,
+    interface: 'tcp://192.168.10.31'
+  });
+
+  // Restante do código de configuração da impressora e impressão
+  printer.alignCenter();
+  printer.setTextSize(1, 1);
+  printer.println("Fichas");
+  printer.newLine();
+  printer.println('-------------------');
+  printer.println(tipoFicha);
+  printer.underlineThick(false);
+  printer.newLine();
+  printer.println(`${ficha}`);
+  printer.newLine();
+  const dataHoraAtual = new Date().toLocaleString();
+  printer.setTextSize(0, 0);
+  printer.println(dataHoraAtual);
+  printer.cut();
+
+  try {
+    printer.execute();
+    console.log("Impressão concluída!");
+    res.send(`Impressão concluída para a ficha de ${tipoFicha} ${ficha}`);
+    db.run('INSERT INTO fichas (tipo, numero, hora) VALUES (?, ?, ?)', tipoFicha, numeroFichas[tipoFicha], dataHoraAtual);
+  } catch (error) {
+    console.error("Falha na impressão:", error);
+    res.status(500).send("Falha na impressão: " + error.message);
+  }
+}
+
 // Rota para gerar fichas de consulta
-router.post('/gerar-ficha-consulta', function (req, res, next) {
+router.post('/consulta', function (req, res, next) {
   gerarFicha("Consulta", res);
 });
 
-router.post('/gerar-ficha-preventivo', function (req, res, next) {
+router.post('/preventivo', function (req, res, next) {
   gerarFicha("Preventivo", res);
 });
 
-router.post('/gerar-ficha-exames-laboratoriais', function (req, res, next) {
+router.post('/exames-laboratoriais', function (req, res, next) {
   gerarFicha("Exames Laboratoriais", res);
 });
 
-router.post('/gerar-ficha-exames-nao-laboratoriais', function (req, res, next) {
+router.post('/exames-nao-laboratoriais', function (req, res, next) {
   gerarFicha("Exames Não Laboratoriais", res);
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [error, setError] = useState(null);
+  const [retrying, setRetrying] = useState(false);
 
   const handleButtonClick = async (tipoFicha) => {
     try {
-      const response = await axios.post(`http://localhost:3000/gerar-ficha-${tipoFicha}`);
-      console.log(response.data); // Manipule a resposta do servidor aqui, se necessário
+      const response = await axios.post(`http://localhost:3000/${tipoFicha}`);
+      console.log(response.data);
+      setError(null); // Limpa qualquer erro anterior
     } catch (error) {
       console.error('Erro na solicitação ao backend:', error);
+      setError('Falha na impressão. Verifique a conexão com a impressora.');
     }
+  };
+
+  const handleRetryClick = async () => {
+    setRetrying(true);
+    setError(null); // Limpa o erro
+
+    try {
+      const response = await axios.get('http://localhost:3000/reconectar');
+      console.log(response.data);
+      setError(null); // Limpa qualquer erro de reconexão
+    } catch (error) {
+      console.error('Erro na tentativa de reconexão:', error);
+      setError('Falha na reconexão com a impressora.');
+    }
+
+    setRetrying(false);
   };
 
   return (
@@ -23,6 +42,12 @@ function App() {
         <button onClick={() => handleButtonClick('exames-laboratoriais')}>EXAMES LABORATORIAIS</button>
         <button onClick={() => handleButtonClick('exames-nao-laboratoriais')}>EXAMES NÃO LABORATORIAIS</button>
       </div>
+      {error && (
+        <div>
+          <p>{error}</p>
+          {!retrying && <button onClick={handleRetryClick}>Tentar Reconexão</button>}
+        </div>
+      )}
       <img src="/logo.png" className="logo" alt="Saude logo" />
       <p className="read-the-docs">By Thiago Cuckasz</p>
     </div>
