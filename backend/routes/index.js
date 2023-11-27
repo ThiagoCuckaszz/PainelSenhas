935d83a273cee24443b5a8f18912a71fd5969b72
ThiagoCuckaszz <pessoalcuckasz@gmail.com>
2023-11-27 11:26:56 -0300

    Painel de chamar Funcionando, falta deixar mais bunito

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -18,7 +18,7 @@ router.post('/exames-laboratoriais', function (req, res, next) {
 });
 
 router.post('/exames-nao-laboratoriais', function (req, res, next) {
-  gerarFicha("Exames Não Laboratoriais", res);
+  gerarFicha("Exames Nao Laboratoriais", res);
 });
 
 
@@ -35,12 +35,12 @@ router.post('/atender_exames-laboratoriais', function (req, res, next) {
 });
 
 router.post('/atender_exames-nao-laboratoriais', function (req, res, next) {
-  atender("Exames Não Laboratoriais", res);
+  atender("Exames Nao Laboratoriais", res);
 });
 
 router.get('/ficha-em-andamento', function (req, res, next) {
-
-  db.all('SELECT tipo, MIN(numero) as lastFicha FROM fichas WHERE atendido = "0" GROUP BY tipo', function (err, fichasEmAndamento) {
+  const dataAtual = new Date().toLocaleDateString();
+  db.all('SELECT tipo, MIN(numero) as lastFicha FROM fichas WHERE atendido = "0" and data = ? GROUP BY tipo',[dataAtual], function (err, fichasEmAndamento) {
     if (err) {
       console.error(err);
       return res.status(500).send('Erro ao consultar o banco de dados');
--- frontend/Painel/src/App.jsx
+++ frontend/Painel/src/App.jsx
@@ -1,33 +1,99 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+const handleRequestError = (error, errorMessage) => {
+  console.error('Erro na solicitação ao backend:', error);
+  setError(errorMessage);
+};
+
+const AtenderButton = ({ tipo, onAtender }) => (
+  <button onClick={() => onAtender(tipo)}>ATENDER {tipo.toUpperCase()}</button>
+);
+
 function App() {
   const [error, setError] = useState(null);
-  const [retrying, setRetrying] = useState(false);
+  const [dados, setDados] = useState([]);
+  const [tipoSelecionado, setTipoSelecionado] = useState('');
 
-
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/ficha-em-andamento');
+      setDados(response.data.fichasEmAndamento);
+      setError(null);
+    } catch (error) {
+      handleRequestError(error, 'Falha ao obter informações de ficha em andamento.');
+    }
+  };
 
   const handleButtonClick = async (tipoFicha) => {
     try {
-      const response = await axios.post(`http://localhost:3000/atender_${tipoFicha}`);
-      console.log(response.data);
-      setError(null); 
+      // Construa a rota a partir do tipo selecionado
+      const rota = `atender_${tipoFicha.toLowerCase().replace(/\s/g, '-')}`;
+      await axios.post(`http://localhost:3000/${rota}`);
+      console.log('Ficha atendida:', tipoFicha);
+      setError(null);
+      fetchData();
     } catch (error) {
-      console.error('Erro na solicitação ao backend:', error);
-      setError('Falha na impressão. Verifique a conexão com a impressora.');
+      handleRequestError(error, 'Falha na impressão. Verifique a conexão com a impressora.');
     }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const exibirInformacoesPorTipo = (tipo) => {
+    const informacoesPorTipo = dados.find(item => item.tipo === tipo);
+
+    if (informacoesPorTipo) {
+      const { tipo, lastFicha } = informacoesPorTipo;
+      return (
+        <div className="card">
+          <p>Tipo: {tipo}</p>
+          <p>Última Ficha: {lastFicha}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="card">
+          <p>Nenhuma informação disponível para o tipo {tipo}.</p>
+          <button onClick={() => fetchData()}>
+            TODAS FICHAS ATENDIDAS ATUALIZAR
+          </button>
+        </div>
+      );
+    }
+  };
+
+  const handleTipoChange = (event) => {
+    setTipoSelecionado(event.target.value);
   };
 
   return (
     <div>
       <h1>Atendimento Senhas</h1>
-      <div className="card">
-        <button onClick={() => handleButtonClick('consulta')}>ATENDER CONSULTA</button>
-        <button onClick={() => handleButtonClick('preventivo')}>ATENDER PREVENTIVO</button>
-        <button onClick={() => handleButtonClick('exames-laboratoriais')}>ATENDER EXAMES LABORATORIAIS</button>
-        <button onClick={() => handleButtonClick('exames-nao-laboratoriais')}>ATENDER EXAMES NÃO LABORATORIAIS</button>
-      </div>
+      {error && <p className="error-message">{error}</p>}
+
+      {/* Lista suspensa para selecionar o tipo */}
+      <label htmlFor="tipoSelect">Selecione o Tipo:</label>
+      <select id="tipoSelect" value={tipoSelecionado} onChange={handleTipoChange}>
+        <option value="">Selecione...</option>
+        <option value="Consulta">Consulta</option>
+        <option value="Exames Laboratoriais">Exames Laboratoriais</option>
+        <option value="Exames Nao Laboratoriais">Exames Nao Laboratoriais</option>
+        <option value="Preventivo">Preventivo</option>
+      </select>
+
+      {tipoSelecionado && exibirInformacoesPorTipo(tipoSelecionado)}
+
+      {/* Botões dinâmicos */}
+      {dados
+        .filter(item => item.tipo === tipoSelecionado)
+        .map((item, index) => (
+          <AtenderButton key={index} tipo={item.tipo} onAtender={handleButtonClick} />
+        ))}
+
       <p className="read-the-docs">By Thiago Cuckasz</p>
     </div>
   );
