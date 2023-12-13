56a1daf4a7f7c3778356f25e31a5ee89f7bd241a
EmanoelViniciusCastanha <111919840+EmanoelViniciusCastanha@users.noreply.github.com>
2023-12-13 14:38:13 -0300

    Back Formatacao

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors'); // Importe o pacote cors
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors"); // Importe o pacote cors
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // Configuração do CORS - Permita acesso a partir de http://localhost:5173
-app.use(cors({
-  origin: '*',
-  methods: 'GET,POST', // Permita métodos GET e POST
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST", // Permita métodos GET e POST
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use(function(req, res, next) {
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 const port = process.env.PORT || 3000;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,60 +1,64 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const sqlite3 = require('sqlite3').verbose();
-const {gerarFicha, atender} = require('./fichasService');
+const sqlite3 = require("sqlite3").verbose();
+const { gerarFicha, atender } = require("./fichasService");
 
-const db = new sqlite3.Database('fichas.db');
+const db = new sqlite3.Database("fichas.db");
 
-router.post('/consulta', function (req, res, next) {
+router.post("/consulta", function (req, res, next) {
   gerarFicha("Consulta", res);
 });
 
-router.post('/preventivo', function (req, res, next) {
+router.post("/preventivo", function (req, res, next) {
   gerarFicha("Preventivo", res);
 });
 
-router.post('/exames-laboratoriais', function (req, res, next) {
+router.post("/exames-laboratoriais", function (req, res, next) {
   gerarFicha("Exames Laboratoriais", res);
 });
 
-router.post('/exames-nao-laboratoriais', function (req, res, next) {
+router.post("/exames-nao-laboratoriais", function (req, res, next) {
   gerarFicha("Exames Nao Laboratoriais", res);
 });
 
-
-router.post('/atender_consulta', function (req, res, next) {
+router.post("/atender_consulta", function (req, res, next) {
   atender("Consulta", res);
 });
 
-router.post('/atender_preventivo', function (req, res, next) {
+router.post("/atender_preventivo", function (req, res, next) {
   atender("Preventivo", res);
 });
 
-router.post('/atender_exames-laboratoriais', function (req, res, next) {
+router.post("/atender_exames-laboratoriais", function (req, res, next) {
   atender("Exames Laboratoriais", res);
 });
 
-router.post('/atender_exames-nao-laboratoriais', function (req, res, next) {
+router.post("/atender_exames-nao-laboratoriais", function (req, res, next) {
   atender("Exames Nao Laboratoriais", res);
 });
 
-router.get('/ultimas-fichas-por-tipo', function (req, res, next) {
-  const tipos = ['Consulta', 'Preventivo', 'Exames Laboratoriais', 'Exames Nao Laboratoriais'];
+router.get("/ultimas-fichas-por-tipo", function (req, res, next) {
+  const tipos = [
+    "Consulta",
+    "Preventivo",
+    "Exames Laboratoriais",
+    "Exames Nao Laboratoriais",
+  ];
   const ultimasFichas = {};
   const dataAtual = new Date().toLocaleDateString();
 
-  tipos.forEach(tipo => {
+  tipos.forEach((tipo) => {
     db.all(
       'SELECT tipo, numero FROM fichas WHERE atendido = "1" AND tipo = ? AND data = ? ORDER BY numero DESC LIMIT 1',
       [tipo, dataAtual],
       function (err, result) {
         if (err) {
           console.error(err);
-          return res.status(500).send('Erro ao consultar o banco de dados');
+          return res.status(500).send("Erro ao consultar o banco de dados");
         }
 
         ultimasFichas[tipo] = result;
-        
+
         // Verifica se já obteve as últimas fichas para todos os tipos
         if (Object.keys(ultimasFichas).length === tipos.length) {
           res.json({ ultimasFichas });
@@ -64,17 +68,20 @@ router.get('/ultimas-fichas-por-tipo', function (req, res, next) {
   });
 });
 
-
-router.get('/ficha-em-andamento', function (req, res, next) {
+router.get("/ficha-em-andamento", function (req, res, next) {
   const dataAtual = new Date().toLocaleDateString();
-  db.all('SELECT tipo, MIN(numero) as lastFicha FROM fichas WHERE atendido = "0" and data = ? GROUP BY tipo',[dataAtual], function (err, fichasEmAndamento) {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Erro ao consultar o banco de dados');
-    }
+  db.all(
+    'SELECT tipo, MIN(numero) as lastFicha FROM fichas WHERE atendido = "0" and data = ? GROUP BY tipo',
+    [dataAtual],
+    function (err, fichasEmAndamento) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Erro ao consultar o banco de dados");
+      }
 
-    res.json({ fichasEmAndamento });
-  });
+      res.json({ fichasEmAndamento });
+    }
+  );
 });
 
 module.exports = router;
